6d0d44ce3a59bc253800b56c1be5e1ab7d6807eb
Lucas Vera <lucazzzv89@gmail.com>
2017-10-19 18:48:52 -0500

    gulp

---
+++ Using modern Javascript Today/GulpApp/public/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var test = 10;
+
+var Person = function Person(name) {
+    _classCallCheck(this, Person);
+
+    this.name = name;
+};
+
+var person1 = new Person('Lucas');
+
+console.log([test, person1]);
---
+++ Using modern Javascript Today/GulpApp/src/scripts/app.js
@@ -0,0 +1,11 @@
+let test = 10;
+
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+const person1 = new Person('Lucas');
+
+console.log([test, person1]);
